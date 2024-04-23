715ab19538e681a4caacb53ce39b489d83ead7c0
Akash Deep Chitransh <85569078+CodeChitra@users.noreply.github.com>
2024-04-23 09:43:58 +0530

    Fix seekbar and many other bugs

--- script.js
+++ script.js
@@ -1,90 +1,68 @@
-let songs = [
-    { "sgName": "Thunder Beats", "sgAuthor": "Johny Stark", "sg": "songs/1.mp3", "sgPhoto": "covers/1.jpg" },
-    { "sgName": "The Unstopabble", "sgAuthor": "DJ Walter", "sg": "songs/2.mp3", "sgPhoto": "covers/2.jpg" },
-    { "sgName": "Too Lusty", "sgAuthor": "Stanis", "sg": "songs/3.mp3", "sgPhoto": "covers/3.jpg" },
-    { "sgName": "Best Friends", "sgAuthor": "Poppins", "sg": "songs/4.mp3", "sgPhoto": "covers/4.jpg" },
-    { "sgName": "Heart Broken", "sgAuthor": "Jack grey", "sg": "songs/5.mp3", "sgPhoto": "covers/5.jpg" },
-    { "sgName": "Thunder Beats", "sgAuthor": "DJ Walter", "sg": "songs/6.mp3", "sgPhoto": "covers/6.jpg" },
-    { "sgName": "Thunder Beats", "sgAuthor": "Stanis", "sg": "songs/7.mp3", "sgPhoto": "covers/7.jpg" },
-    { "sgName": "Thunder Beats", "sgAuthor": "Moon Crystal", "sg": "songs/8.mp3", "sgPhoto": "covers/8.jpg" },
-    { "sgName": "Thunder Beats", "sgAuthor": "Poppins", "sg": "songs/9.mp3", "sgPhoto": "covers/9.jpg" },
-    { "sgName": "Thunder Beats", "sgAuthor": "Johny Stark", "sg": "songs/10.mp3", "sgPhoto": "covers/10.jpg" }
-]
-
-// Song PLay Pause Functionality
-let i = 0;
-let playBt = document.getElementById("play-pause");
-let audioElem = new Audio(songs[i].sg);
+let audioElem = new Audio();
 let seekBar = document.getElementById("seekBar");
-
-playBt.addEventListener("click", () => {
-
-    if (playBt.classList.contains("fa-circle-play")) {
-        playBt.classList.remove("fa-circle-play");
-        playBt.classList.add("fa-circle-pause");
-        audioElem.play();
-        start();
-    }
-    else {
-        playBt.classList.remove("fa-circle-pause");
-        playBt.classList.add("fa-circle-play");
-        audioElem.pause();
-    }
-})
-
-
-function start() {
-
-    audioElem.addEventListener("timeupdate", () => {
-        let progress = parseInt((audioElem.currentTime / audioElem.duration) * 100);
-        seekBar.value = progress;
-    })
-
-}
-seekBar.addEventListener("change", () => {
-    audioElem.currentTime = ((seekBar.value * audioElem.duration) / 100);
-})
-
-// Music Change Functionality
-
+let playBt = document.getElementById("play-pause");
 let forward = document.getElementById("forward");
 let backward = document.getElementById("backward");
 let naam = document.getElementById("naam");
 let author = document.getElementById("author");
 let photo = document.getElementById("photo");
+let i = 0;
 
+// Function to load a new song
+function loadSong(index) {
+    audioElem.src = songs[index].sg;
+    naam.innerText = songs[index].sgName;
+    author.innerText = songs[index].sgAuthor;
+    photo.src = songs[index].sgPhoto;
+}
+
+// Function to play or pause the song
+function togglePlay() {
+    if (audioElem.paused || audioElem.ended) {
+        playBt.classList.remove("fa-circle-play");
+        playBt.classList.add("fa-circle-pause");
+        audioElem.play();
+    } else {
+        playBt.classList.remove("fa-circle-pause");
+        playBt.classList.add("fa-circle-play");
+        audioElem.pause();
+    }
+}
+
+// Event listener for play/pause button
+playBt.addEventListener("click", togglePlay);
+
+// Event listener for updating the seekbar
+audioElem.addEventListener("timeupdate", () => {
+    let progress = (audioElem.currentTime / audioElem.duration) * 100;
+    seekBar.value = progress;
+});
+
+// Event listener for seeking
+seekBar.addEventListener("change", () => {
+    audioElem.currentTime = (seekBar.value * audioElem.duration) / 100;
+});
+
+// Event listener for forward button
 forward.addEventListener("click", () => {
-    if (i < 10) {
-
+    if (i < songs.length - 1) {
         i++;
+        loadSong(i);
+        togglePlay();
+    } else {
+        i = 0; // Wrap around to the beginning of the playlist
+        loadSong(i);
+        togglePlay();
     }
-    audioElem.pause();
-    audioElem = new Audio(songs[i].sg);
+});
 
-    audioElem.currentTime = 0;
-    start();
-    audioElem.play();
-    naam.innerText = songs[i].sgName;
-    author.innerText = songs[i].sgAuthor;
-    photo.src = songs[i].sgPhoto;
-    playBt.classList.remove("fa-circle-play");
-    playBt.classList.add("fa-circle-pause");
-})
-
+// Event listener for backward button
 backward.addEventListener("click", () => {
-    if (i > 0) {
-
+    if (audioElem.currentTime > 3 || i === 0) {
+        audioElem.currentTime = 0; // Restart the song if more than 3 seconds have passed
+    } else {
         i--;
+        loadSong(i);
+        togglePlay();
     }
-    audioElem.pause();
-    audioElem = new Audio(songs[i].sg);
-    start()
-    audioElem.play();
-    // audioElem.currentTime = 0;
-    naam.innerText = songs[i].sgName;
-    author.innerText = songs[i].sgAuthor;
-    photo.src = songs[i].sgPhoto;
-    playBt.classList.remove("fa-circle-play");
-    playBt.classList.add("fa-circle-pause");
-})
-
+});
